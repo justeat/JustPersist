b56ed211d886e399eee63d3b0e4fadfb5f5fbed5
Alberto De Bortoli <albertodebortoli@gmail.com>
2017-11-03 16:49:55 +0000

    Remove the Pods folder

--- Example/Pods/Target Support Files/Pods-JustPersist_watchOS_Example Extension/Pods-JustPersist_watchOS_Example Extension-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_JustPersist_watchOS_Example_Extension : NSObject
-@end
-@implementation PodsDummy_Pods_JustPersist_watchOS_Example_Extension
-@end
